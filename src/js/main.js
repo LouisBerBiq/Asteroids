11854e5a3347e59c138face0b1f98acbc45bff52
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-03-24 14:05:46 +0100

    fixed looping update and added ship obj

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import ship from "./ship.js";
+
 const NoJsMessage = document.querySelector('#no-js');
 document.body.removeChild(NoJsMessage);
 
@@ -8,23 +10,52 @@ canvas.width = 640;
 canvas.height = 480;
 
 // TODO?: check if browser supports canvas in the first place
-const CanvasContext2D = canvas.getContext('2d');
-CanvasContext2D.strokeStyle = '#FFF';
+const canvasContext2D = canvas.getContext('2d');
+canvasContext2D.strokeStyle = '#FFF';
 
 const shipSize = 20;
+const AsteroidSize = 20;
+const shipSpeed = 1;
+let shipPosition = {
+	x: canvas.width / 2,
+	y: canvas.height / 2
+}
 
-CanvasContext2D.translate(canvas.width / 2, canvas.height / 2)
+function shipUpdate() {
+	shipPosition.x += shipSpeed;
+	if (shipPosition.x > canvas.width) {
+		
+	}
+	drawSpaceship();
+}
 
-// spaceship model
-CanvasContext2D.beginPath();
-CanvasContext2D.moveTo(0, -1.5 * (shipSize / 2));
-CanvasContext2D.lineTo(shipSize / 2, 1.5 * (shipSize / 2));
-CanvasContext2D.lineTo(-shipSize / 2, 1.5 * (shipSize / 2));
-CanvasContext2D.closePath();
+function drawSpaceship() {
+	// 30px high / 20px wide
+	canvasContext2D.save();
+	canvasContext2D.translate(shipPosition.x, shipPosition.y);
+	canvasContext2D.beginPath();
+	canvasContext2D.moveTo(0, -1.5 * (shipSize / 2));
+	canvasContext2D.lineTo(shipSize / 2, 1.5 * (shipSize / 2));
+	canvasContext2D.lineTo(-shipSize / 2, 1.5 * (shipSize / 2));
+	canvasContext2D.closePath();
+	canvasContext2D.stroke();
+	canvasContext2D.restore();
+}
 
-CanvasContext2D.stroke();
+function drawAsteroid() {
+	canvasContext2D.save();
+	canvasContext2D.translate(50, 50);
+	canvasContext2D.strokeRect(50, 50, AsteroidSize, AsteroidSize)
+	canvasContext2D.stroke();
+	canvasContext2D.restore();
+}
+
 
 function update() {
-
-	window.requestAnimationFrame((update()));
+	canvasContext2D.clearRect(0, 0, canvas.width, canvas.height)
+	shipUpdate();
+	
+	window.requestAnimationFrame(update);
 }
+
+update();
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,5 +3,6 @@ mix.disableNotifications();
 
 mix .setPublicPath('dist/')
 	.copy('src/index.html', 'dist/index.html') 
+	// .copy('src/js/main.js', 'dist/js') 
 	.js('src/js/main.js', 'dist/js') 
 	.sass('src/scss/main.scss', 'dist/css');
